8adda8a202009a9e2f17ca01996c36b080000c3d
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-06-26 11:18:38 +0200

    wykonywane krok po kroku - 3.AsyncAndAwait

--- 3.AsyncAndAwait/app/src/main/java/pl/klenczi/asyncandawait/MainActivity.kt
+++ 3.AsyncAndAwait/app/src/main/java/pl/klenczi/asyncandawait/MainActivity.kt
@@ -1,6 +1,7 @@
 package pl.klenczi.asyncandawait
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,13 +11,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import pl.klenczi.asyncandawait.ui.theme.AsyncAndAwaitTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.i("Main", "--Calculation started...")
+            val stock1 = getStock1()
+            val stock2 = getStock2()
+            val total = stock1 + stock2
+            Log.i("Main", "--total: $total")
         }
+        setContent {}
     }
+}
+
+private suspend fun getStock1(): Int{
+    delay(10000)
+    Log.i("Main", "--stock 1 returned")
+    return 55000
+}
+
+private suspend fun getStock2(): Int{
+    delay(8000)
+    Log.i("Main", "--stock 2 returned")
+    return 35000
 }
