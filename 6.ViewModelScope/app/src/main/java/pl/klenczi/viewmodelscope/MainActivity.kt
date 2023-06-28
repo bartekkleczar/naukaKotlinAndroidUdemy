95aa7e0968d54e969f859b027653d4030377d9a1
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-06-28 13:36:18 +0200

    done - 6.ViewModelScope

--- 6.ViewModelScope/app/src/main/java/pl/klenczi/viewmodelscope/MainActivity.kt
+++ 6.ViewModelScope/app/src/main/java/pl/klenczi/viewmodelscope/MainActivity.kt
@@ -1,6 +1,7 @@
 package pl.klenczi.viewmodelscope
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,13 +11,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import pl.klenczi.viewmodelscope.ui.theme.ViewModelScopeTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var mainActivityViewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-
-        }
+        mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        mainActivityViewModel.getUserData()
+        mainActivityViewModel.users.observe(this, Observer {myUsers ->
+            myUsers.forEach{
+                Log.i("Main", "name: ${it.name}")
+            }
+        })
     }
 }
