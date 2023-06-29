1a2e1edafa49f7d1cb9fdb238c5293270764e897
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-06-29 20:09:08 +0200

    initial - 8.LiveDataBuilder

---
+++ 8.LiveDataBuilder/app/src/main/java/pl/klenczi/livedatabuilder/MainActivityViewModel.kt
@@ -0,0 +1,22 @@
+package pl.klenczi.livedatabuilder
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivityViewModel : ViewModel() {
+    private var userRepository = UserRepository()
+    var users: MutableLiveData<List<User>> = MutableLiveData()
+    fun getUserData() {
+        viewModelScope.launch {
+            var result: List<User>? = null
+            withContext(Dispatchers.IO){
+                result = userRepository.getUsers()
+            }
+            users.value = result
+        }
+    }
+}
