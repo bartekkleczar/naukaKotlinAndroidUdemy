5d84660f846934fa090a2a77899851e9abcd1d0c
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-07-12 21:17:46 +0200

    done

--- app/src/main/java/pl/klenczi/work_manager/MainActivity.kt
+++ app/src/main/java/pl/klenczi/work_manager/MainActivity.kt
@@ -1,22 +1,95 @@
 package pl.klenczi.work_manager
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import pl.klenczi.work_manager.ui.theme.WorkmanagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            val textStateFlow = MutableStateFlow("")
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth(0.7f)
+                        .height(100.dp),
+                    onClick = {
+                        lifecycleScope.launch {
+                            setOneTimeWorkRequest()
+                        }
+                    }
+                ) {
+                    Text(
+                        text = "Run Worker",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp
+                    )
+                }
+            }
         }
+    }
+
+    private suspend fun setOneTimeWorkRequest() {
+        val workManager = WorkManager.getInstance(applicationContext)
+        val uploadWorkRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .build()
+        workManager.enqueue(uploadWorkRequest)
+        workManager.getWorkInfoByIdLiveData(uploadWorkRequest.id).asFlow()
+            .collect{ value ->
+                Log.i("Main", value.state.name)
+            }
+    }
+}
+
+
+fun <T> LiveData<T>.asFlow(): Flow<T> = channelFlow {
+    val observer = Observer<T> { value ->
+        trySend(value).isSuccess // Wysyłanie wartości ze "live data" do kanału Flow
+    }
+    observeForever(observer)
+
+    awaitClose {
+        removeObserver(observer)
     }
 }
